2b17c89134f180836a8b0077c73ef040f9755c99
openred <openredshaw@163.com>
2018-09-26 19:13:43 +0800

    Grab movie data from douban api
    创建koa服务器进程，抓取豆瓣电影的数据并存储在MongoDB数据库。
    （注意：豆瓣电影的API接口可能会更改，记得动态更改api.js）。

---
+++ iMovieServer/crawler/trainer-list.js
@@ -0,0 +1,58 @@
+const puppeteer = require('puppeteer')
+
+const url = `https://movie.douban.com/tag/#/?sort=U&range=6,10&tags=`
+
+// 延时函数
+const sleep = time => new Promise(resolve =>  {
+  setTimeout(resolve, time)
+})
+
+;(async () => {
+  console.log('Start visit the target page')
+
+  // 创建爬虫
+  const browser = await puppeteer.launch({
+    executablePath: './chromium/chrome.exe',
+    args: ['--no-sanbox'],
+    dumpio: false
+  })
+
+  const page = await browser.newPage()
+  await page.goto(url, {
+    waitUntil: 'networkidle2'
+  })
+
+  await sleep(3000)   // 等待3s网页加载结束
+
+  await page.waitForSelector('.more')
+
+  for(let i=0; i<3; i++) {
+    await sleep(3000)
+    console.log('click more ',i)
+    await page.click('.more')
+  }
+
+  const result = await page.evaluate(() => {
+    var $ = window.$
+    var items = $('.list-wp a')
+    var links = []
+
+    if (items.length >= 1) {
+      items.each((index, item) => {
+        let it = $(item)
+        let doubanId = it.find('div').data('id')
+        let title = it.find('.title').text()
+        let rate = Number(it.find('.rate').text())
+        let poster = it.find('img').attr('src').replace('s_ratio', 'l_ratio')
+
+        links.push({doubanId, title, rate, poster})
+      })
+    }
+    return links
+  })
+
+  browser.close()
+  // console.log(result)
+  process.send({result})
+  process.exit(0)
+})()
---
+++ iMovieServer/database/schema/movie.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const { ObjectId, Mixed } = Schema.Types
+
+const movieSchema = new Schema({
+  doubanId: { unique: true, type: String },
+  category: [ { type: ObjectId, ref: 'Category' } ],
+  rate: Number,
+  title: String,
+  original_title: String,
+  countries: [String],
+  summary: String,
+  year: String,
+  genres: [String],
+  directors: [
+    { alt: String, name: String, id: String }
+  ],
+  aka: [String],
+  subtype: String,
+  comments_count: Number,
+  ratings_count: Number,
+
+  video: String,
+  poster: String,
+  cover: String,
+  videoKey: String,
+  posterKey: String,
+  coverKey: String,
+
+  meta: {
+    createAt: { type: Date, default: Date.now() },
+    updateAt: { type: Date, default: Date.now() },
+  }
+})
+
+movieSchema.pre('save', function (next) {  //中间件，判断数据是否为新数据
+  if (this.isNew) {
+    this.meta.createAt = this.meta.updateAt = Date.now()
+  } else {
+    this.meta.updateAt = Date.now()
+  }
+
+  next()
+})
+
+mongoose.model('Movie', movieSchema)
